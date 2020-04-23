ed9d125d083dbd628576e09958575a9152efa3d2
chaim <chenming@dayuwuxian.com>
2020-04-23 10:35:05 +0800

    rm go

--- redisp.go
+++ redisp.go
@@ -68,9 +68,9 @@ func main() {
 				for cmd := range cmdQueue {
 					key, val, duration := string(cmd.Args[1]), cmd.Args[2], 0*time.Second
 					err := sourceClient.Set(key, val, duration).Err()
-					// if err == nil {
-					// 	err = targetClient.Set(key, val, duration).Err()
-					// }
+					if err == nil {
+						err = targetClient.Set(key, val, duration).Err()
+					}
 					if err != nil {
 						log.Printf("Set key: %s value: %s failed, err: %s", key, val, err)
 					}
